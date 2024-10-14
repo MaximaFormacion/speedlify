82eea9a8ffd03526cbeab476f0d2ab4c485bd50a
MaximaFormacion <maximaformacion@albinsoft.es>
2024-10-14 21:02:46 +0200

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -6,6 +6,13 @@ module.exports = {
 		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://www.maximaformacion.es/blog-ciencia-datos/",
+		"https://www.maximaformacion.es/blog-teleformacion/",
+		"https://www.maximaformacion.es/curso/",
+		"https://www.maximaformacion.es/curso/curso-moodle-para-docentes-con-exelearning/",
+		"https://www.maximaformacion.es/",
+		"https://www.maximaformacion.es/masters/master-de-estadistica-aplicada-con-r-software/",
+		"https://www.maximaformacion.es/masters/",
+		"https://www.maximaformacion.es/recursos/"
 	]
 };
